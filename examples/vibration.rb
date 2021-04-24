692cd15f5fcd545fe5caa697314962859fb77b10
frissyn <62220201+frissyn@users.noreply.github.com>
2021-04-24 09:52:08 -0400

    ⚡: update module name

--- examples/vibration.rb
+++ examples/vibration.rb
@@ -2,19 +2,19 @@ require 'rxi'
 
 # Get Player 0's Controller and create an input state
 port = 0
-state = Gamepad::XINPUT_STATE.new
+state = RXI::XINPUT_STATE.new
 
-# Create a vibration struct to control motor's
-vibration = Gamepad::XINPUT_VIBRATION.new
+# Create a vibration struct to control motors
+vibration = RXI::XINPUT_VIBRATION.new
 
-# Set left and right motor's to a low speed (16383)
-vibration[:wLeftMotorSpeed] = Gamepad::Vibration::LOWER
-vibration[:wRightMotorSpeed] = Gamepad::Vibration::LOWER
+# Set left and right motors to a low speed (16383)
+vibration[:wLeftMotorSpeed] = RXI::Vibration::LOWER
+vibration[:wRightMotorSpeed] = RXI::Vibration::LOWER
 
 # Enable state getting/setting
 RXI.enable(true)
 
 # Infinitely update controller state
 loop do
-    puts Gamepad.set_state(port, vibration)
+    puts RXI.set_state(port, vibration)
 end
